af5414f3f3543718b1b31ba3da7090603209a419
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-12 19:42:13 -0400

    Updates

---
+++ precache-manifest.9fdd810b67a1d86544e4af90c47463bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af12226a508fd0b04269772dd72e1a63",
+    "url": "/Dev/index.html"
+  },
+  {
+    "revision": "9f963ce6752f1b04fdf3",
+    "url": "/Dev/static/css/main.e5d629a1.chunk.css"
+  },
+  {
+    "revision": "9e2201d42385263fdd85",
+    "url": "/Dev/static/js/2.65da6242.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Dev/static/js/2.65da6242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f963ce6752f1b04fdf3",
+    "url": "/Dev/static/js/main.8536a144.chunk.js"
+  },
+  {
+    "revision": "941218be41b040ccfe66",
+    "url": "/Dev/static/js/runtime-main.06876827.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Dev/static/media/logo.5d5d9eef.svg"
+  }
+]);
