dc55932c1d39b48615de67464fbfdc3d55746451
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-12 16:21:43 -0400

    yay

--- src/Components/MyNavBar/MyNavBar.jsx
+++ src/Components/MyNavBar/MyNavBar.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Collapse from "react-bootstrap/Collapse";
 import NavStyle from "./MyNavBar.module.css";
@@ -13,13 +16,13 @@ class MyNavBar extends Component {
 
   componentDidMount() {
     window.addEventListener("scroll", this.onscroll);
-    console.log("componentDidMount : ", this.state.open);
+    // console.log("componentDidMount : ", this.state.open);
   }
   componentWillUnmount() {
     window.removeEventListener("scroll", this.onscroll);
   }
   componentDidUpdate() {
-    console.log("componentDidUpdate: ", this.state.open);
+    // console.log("componentDidUpdate: ", this.state.open);
   }
 
   onscroll = () => {
@@ -87,30 +90,48 @@ class MyNavBar extends Component {
           <div className={NavStyle.navbar}>
             <div className={NavStyle.buttonWrapper}>
               {/* WRAPPER */}
-
-              {/* BUTTON */}
-              <Button
-                className={NavStyle.menuButton}
-                aria-controls="example-collapse-text"
-                aria-expanded={this.state.open}
-                onClick={this.toggleCollapse}
-              >
-                <div className={NavStyle.menuIconContainer}>
-                  <div
-                    className={NavStyle.bar1}
-                    style={this.state.changeNavBar ? changeBar1 : bar}
-                  ></div>
-                  <div
-                    className={NavStyle.bar2}
-                    style={this.state.changeNavBar ? changeBar2 : bar}
-                  ></div>
-                  <div
-                    className={NavStyle.bar3}
-                    style={this.state.changeNavBar ? changeBar3 : bar}
-                  ></div>
-                </div>
-              </Button>
-              {/* BUTTON */}
+              <Row>
+                <Col className={NavStyle.name}>
+                  <a href="/" className={NavStyle.a}>
+                    Jesse McKinney
+                  </a>
+                </Col>
+                <Col className={NavStyle.projects}>
+                  <a href="/" className={NavStyle.a}>
+                    Projects
+                  </a>
+                </Col>
+                <Col className={NavStyle.contact}>
+                  <a href="/" className={NavStyle.a}>
+                    Contact
+                  </a>
+                </Col>
+                <Col>
+                  {/* BUTTON */}
+                  <Button
+                    className={NavStyle.menuButton}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={this.state.open}
+                    onClick={this.toggleCollapse}
+                  >
+                    <div className={NavStyle.menuIconContainer}>
+                      <div
+                        className={NavStyle.bar1}
+                        style={this.state.changeNavBar ? changeBar1 : bar}
+                      ></div>
+                      <div
+                        className={NavStyle.bar2}
+                        style={this.state.changeNavBar ? changeBar2 : bar}
+                      ></div>
+                      <div
+                        className={NavStyle.bar3}
+                        style={this.state.changeNavBar ? changeBar3 : bar}
+                      ></div>
+                    </div>
+                  </Button>
+                  {/* BUTTON */}
+                </Col>
+              </Row>
 
               {/* COLLAPSE CONTENT */}
               <Collapse in={this.state.open}>
@@ -118,10 +139,82 @@ class MyNavBar extends Component {
                   id="example-collapse-text"
                   className={NavStyle.collapsingContent}
                 >
-                  <ul className={NavStyle.ul}>
-                    <li>Hello</li>
-                    <li>World</li>
-                  </ul>
+                  <div className={NavStyle.contentDiv}>
+                    <Container className={NavStyle.contentContainer}>
+                      <Row className={NavStyle.contentHeader}>
+                        <h3 className={NavStyle.h3}>Project Links</h3>
+                      </Row>
+                      <hr />
+                      <Row className={NavStyle.contentRow}>
+                        <Col className={NavStyle.contentCol}>
+                          <ul className={NavStyle.ul}>
+                            <li className={NavStyle.li}>
+                              <a href="/" className={NavStyle.a}>
+                                Infinity Stars
+                              </a>
+                            </li>
+                            <li className={NavStyle.li}>
+                              <a href="/" className={NavStyle.a}>
+                                Infinity Box
+                              </a>
+                            </li>
+                            <li className={NavStyle.li}>
+                              <a href="/" className={NavStyle.a}>
+                                Thanos Snap Effect
+                              </a>
+                            </li>
+                          </ul>
+                        </Col>
+                        <Col className={NavStyle.contentCol}>
+                          <ul className={NavStyle.ul}>
+                            <li className={NavStyle.li}>
+                              <a href="/" className={NavStyle.a}>
+                                Confetti Effect
+                              </a>
+                            </li>
+                            <li className={NavStyle.li}>
+                              <a href="/" className={NavStyle.a}>
+                                Cracked Font
+                              </a>
+                            </li>
+                            <li className={NavStyle.li}>
+                              <a href="/" className={NavStyle.a}>
+                                Curved Container
+                              </a>
+                            </li>
+                          </ul>
+                        </Col>
+                        <Col className={NavStyle.contentCol}>
+                          <ul className={NavStyle.ul}>
+                            <li className={NavStyle.li}>
+                              <a href="/" className={NavStyle.a}>
+                                Real Water Effect
+                              </a>
+                            </li>
+                            <li className={NavStyle.li}>
+                              <a href="/" className={NavStyle.a}>
+                                SimilTune
+                              </a>
+                            </li>
+                          </ul>
+                        </Col>
+                        <Col className={NavStyle.contentCol}>
+                          <ul className={NavStyle.ul}>
+                            <li className={NavStyle.li}>
+                              <a href="/" className={NavStyle.a}>
+                                Reflective 3D Ball
+                              </a>
+                            </li>
+                            <li className={NavStyle.li}>
+                              <a href="/" className={NavStyle.a}>
+                                Perspective Font
+                              </a>
+                            </li>
+                          </ul>
+                        </Col>
+                      </Row>
+                    </Container>
+                  </div>
                 </div>
               </Collapse>
               {/* COLLAPSE CONTENT */}
