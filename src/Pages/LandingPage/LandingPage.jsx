aade9ec720d83e5a0f1a013610ea8dfa6adc076c
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-12 20:30:15 -0400

    yay

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import NavBar from "../../Components/MyNavBar/MyNavBar";
 import Footer from "../../Components/MyFooter/MyFooter";
 import Style from "./LandingPage.module.css";
@@ -9,9 +10,13 @@ function LandingPage(props) {
     <>
       <div className={Style.landingWrapper}>
         <NavBar />
-        <Container className={Style.container}></Container>
-        <Footer />
+        <Container fluid className={Style.container}>
+          <Row className={Style.introRow}>
+            <div className="col-md-6"></div>
+          </Row>
+        </Container>
       </div>
+      <Footer />
     </>
   );
 }
