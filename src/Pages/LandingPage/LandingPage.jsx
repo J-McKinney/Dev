a1e53784cf73e49abcd6c7ceab66657505cebdfc
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-13 10:54:13 -0400

    yay

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
+import Row from "react-bootstrap/Row";
 import NavBar from "../../Components/MyNavBar/MyNavBar";
 import MyCarousel from "../../Components/MyCarousel/MyCarousel";
+import ATL from "../../images/atlanta.jpg";
 import Footer from "../../Components/MyFooter/MyFooter";
 import Style from "./LandingPage.module.css";
 
@@ -11,7 +12,16 @@ function LandingPage(props) {
     <>
       <div className={Style.landingWrapper}>
         <NavBar />
-        <Container fluid className={Style.container}></Container>
+        <Container fluid className={Style.container}>
+          <Row className={Style.row}>
+            <div id={Style.name} className="col-md-9">
+              <h2 className={Style.h2Name}>Jesse McKinney</h2>
+            </div>
+            <div id={Style.atl} className="col-md-3">
+              <img className={Style.atlanta} src={ATL} alt="ATL City" />
+            </div>
+          </Row>
+        </Container>
       </div>
       <MyCarousel />
       <Footer />
