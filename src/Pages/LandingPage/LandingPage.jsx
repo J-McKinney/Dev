8c53755fe90866d1ecefd76125a23b48d8d9a85c
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-12 21:22:08 -0400

    yay

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import NavBar from "../../Components/MyNavBar/MyNavBar";
+import MyCarousel from "../../Components/MyCarousel/MyCarousel";
 import Footer from "../../Components/MyFooter/MyFooter";
 import Style from "./LandingPage.module.css";
 
@@ -13,8 +14,12 @@ function LandingPage(props) {
         <Container fluid className={Style.container}>
           <Row className={Style.introRow}>
             <div className="col-md-6"></div>
+            <div className="col-md-6"></div>
           </Row>
         </Container>
+        <div className={Style.carouselContainer}>
+          <MyCarousel />
+        </div>
       </div>
       <Footer />
     </>
