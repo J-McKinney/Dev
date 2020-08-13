841f1866b67c8c17882283580da260bcfc267cc0
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-13 14:48:38 -0400

    yay

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -25,8 +25,8 @@ function LandingPage(props) {
                 free website tutorials and soon decided to go back to school for
                 a more advanced approach on the subject. Before I knew it, I was
                 creating full websites and programs. I'm now a full-time Full
-                Stack Flex Web Developer working in Freelance, looking for a
-                full-time job opportunity.
+                Stack Flex Web Developer working freelance and looking for a
+                full-time job opportunity to further my experience and career.
               </p>
             </div>
             <div id={Style.atl} className="col-md-3">
@@ -38,10 +38,26 @@ function LandingPage(props) {
       <MyCarousel />
 
       <div className={Style.projectWrapper}>
-        <Container fluid className={Style.projectContainer}>
+        <Container
+          id="project-section"
+          fluid
+          className={Style.projectContainer}
+        >
           <Row className={Style.projectRow}></Row>
         </Container>
       </div>
+
+      <svg
+        className={Style.svg}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          fill="#282c34"
+          fill-opacity="1"
+          d="M0,256L48,261.3C96,267,192,277,288,256C384,235,480,181,576,181.3C672,181,768,235,864,256C960,277,1056,267,1152,266.7C1248,267,1344,277,1392,282.7L1440,288L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+        ></path>
+      </svg>
 
       <Footer />
     </>
